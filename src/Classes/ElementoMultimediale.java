d063b4fae702d8f1d02f0c3eb2de4a9a0cf27f04
Federico Poggi <federicopoggi3@gmail.com>
2023-12-01 16:53:43 +0100

    corretto ElementoMultimediale

--- src/Classes/ElementoMultimediale.java
+++ src/Classes/ElementoMultimediale.java
@@ -20,31 +20,4 @@ public abstract class ElementoMultimediale{
 
     abstract void alzaLuminosita();
 
-    public void createElement(String type){
-        switch (type){
-            case "immagine":
-                String immagineTitle;
-                int imgBrightness;
-                System.out.println("Inserisci titolo immagine:");
-                immagineTitle=scan.nextLine();
-                imgBrightness=scan.nextInt();
-                new Immagine(immagineTitle, imgBrightness);
-                System.out.println("Immagine creata");
-
-            case "video":
-                String videoTitle;
-                int videoBright;
-                int volume;
-                System.out.println("Inserisci titolo del video:");
-                videoTitle=scan.nextLine();
-                System.out.println("inserisci una luminosita da 1 a 10");
-                videoBright=scan.nextInt();
-                System.out.println("inserisci un volume da 1 a 10");
-                volume=scan.nextInt();
-                new Video(videoTitle,videoBright,volume);
-                System.out.println("Video Creato");
-        }
-    }
-
-
 }
--- src/Classes/Interface/MainInteface.java
+++
@@ -1,5 +0,0 @@
-package Classes.Interface;
-
-public interface MainInteface {
-    public void createElement();
-}
